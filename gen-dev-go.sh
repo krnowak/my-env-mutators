e1549d12a09fca25a606be866a2e0c7a9f3f1b11
Krzesimir Nowak <qdlacz@gmail.com>
2017-10-17 13:14:48 +0200

    Locate go repositories in the same directory as the script itself

--- gen-dev-go.sh
+++ gen-dev-go.sh
@@ -10,17 +10,20 @@ else
 fi
 
 fn="dev${version}.inc"
+dir=$(realpath $(dirname "${0}"))
 
 cat <<EOF >"${fn}"
 LKSJHDFSKDH_GO_VERSION="${version}"
 LKSJHDFSKDH_PWD="${PWD}"
+LKSJHDFSKDH_GO_DIR="${dir}"
 EOF
 
 cat <<'EOF' >>"${fn}"
 export GOPATH="${LKSJHDFSKDH_PWD}/GOPATH"
-add_to_env_var PATH go "${HOME}/projects/go/go-${LKSJHDFSKDH_GO_VERSION}/bin:${GOPATH}/bin"
+add_to_env_var PATH go "${LKSJHDFSKDH_GO_DIR}/go-${LKSJHDFSKDH_GO_VERSION}/bin:${GOPATH}/bin"
 add_to_env_var PS1DATA go "go-${LKSJHDFSKDH_GO_VERSION}, $(basename ${LKSJHDFSKDH_PWD})"
 
+unset LKSJHDFSKDH_GO_DIR
 unset LKSJHDFSKDH_PWD
 unset LKSJHDFSKDH_GO_VERSION
 EOF
